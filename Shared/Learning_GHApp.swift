c5aa91d5a95a360ddb159396015a2b51d4d78836
Tito <red_man1978@hotmail.com>
2022-09-22 16:08:01 +0200

    Initial Commit

---
+++ Shared/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Shared
+//
+//  Created by Tito on 22/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
